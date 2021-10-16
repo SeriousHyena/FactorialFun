ffd2c5ab0b8643c5514c937d46a1426df0892ab7
SeriousHyena <emackiii@gmail.com>
2021-10-15 17:16:05 -0700

    progress

--- main.cpp
+++ main.cpp
@@ -3,8 +3,43 @@
 
 using namespace std;
 
+//declare functions here
+int factorial(int num);
+int counted(int& count); 
+
+int fact2;
+int& count = 4;
+
 int main()
 {
+	int counted(int& count);	//invoke
+	int factorial(int num);		//invoke the function
+	int fact = factorial(4);
+	cout << fact << endl;
+	
+
+	
 
 	return 0;
+}
+
+//place functions here
+int factorial(int num)
+{
+	if (num >= 0)
+	{
+		counted(count-1);
+		return num * (num - 1);
+
+		
+		
+		
+	}
+	return num;
+}
+
+int counted(int& count)
+{
+
+	return count--;
 }
